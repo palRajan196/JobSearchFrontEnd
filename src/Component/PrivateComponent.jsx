27dc6453f93a8d82ba7c4a97e8a9911e15c41c56
palRajan196 <palrajan196@gmail.com>
2025-02-15 12:03:15 +0530

    useEffect defined

--- src/Component/PrivateComponent.jsx
+++ src/Component/PrivateComponent.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffects } from "react";
+import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function PrivateComponent() {
