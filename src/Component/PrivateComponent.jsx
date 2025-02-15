61ffe6037d113791b76041ddced57496b91db977
palRajan196 <palrajan196@gmail.com>
2025-02-15 12:54:12 +0530

    Private componants error

--- src/Component/PrivateComponent.jsx
+++ src/Component/PrivateComponent.jsx
@@ -3,16 +3,21 @@ import { useEffect } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function PrivateComponent() {
-  let auth;
-  useEffect(()=>{
-    const login = localStorage.getItem("user");
-    if(login=="" || login=="Login"){
+ let auth;
+    auth = localStorage.getItem("user");
+    if(auth==""){
+      console.log("auth"+auth);
       localStorage.setItem("user","Login");
     }
-  })
 
   auth = localStorage.getItem("user");
-  return auth != "Login" ? <Outlet /> : <Navigate to="/Login" />;
+  // return auth != "Login" ? <Outlet /> : <Navigate to="/Login" />;
+  return(
+    <>
+    {auth != "Login" ? <Outlet /> : <Navigate to="/Login" />};
+    
+    </>
+  )
 }
 
 export default PrivateComponent;
