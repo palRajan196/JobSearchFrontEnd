ec97f52783ce7d6624e0d77892f59e26c9f2f617
palRajan196 <palrajan196@gmail.com>
2025-02-15 16:48:23 +0530

    Private Componantes

--- src/Component/PrivateComponent.jsx
+++ src/Component/PrivateComponent.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { Navigate, Outlet } from "react-router-dom";
 
+
 function PrivateComponent() {
+  const navigate = useNavigate();
  let auth;
     auth = localStorage.getItem("user");
     if(auth==""){
       console.log("auth"+auth);
       localStorage.setItem("user","Login");
+      navigate("/Login");
     }
 
   auth = localStorage.getItem("user");
-  // return auth != "Login" ? <Outlet /> : <Navigate to="/Login" />;
+  
   return(
     <>
     {auth != "Login" ? <Outlet /> : <Navigate to="/Login" />};
