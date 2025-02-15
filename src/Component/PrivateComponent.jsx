5a4012ebb4384cb228435b16ee78f43b555e75c0
palRajan196 <palrajan196@gmail.com>
2025-02-15 11:45:38 +0530

    finish some error on Private componenets

--- src/Component/PrivateComponent.jsx
+++ src/Component/PrivateComponent.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 function PrivateComponent() {
-  const auth = localStorage.getItem("user");
+  let auth;
+  useEffect(()=>{
+    const login = localStorage.getItem("user");
+    if(login=="" || login=="Login"){
+      localStorage.setItem("user","Login");
+    }
+  })
+
+  auth = localStorage.getItem("user");
   return auth != "Login" ? <Outlet /> : <Navigate to="/Login" />;
 }
 
